bf298b8c7b55ab5c5fd815c50aab0099f51f2af8
Andreas Longo <git@andreaslongo.com>
2024-07-18 17:08:57 +0200

    Prepare some basic tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -7,10 +7,9 @@
 #![warn(missing_docs)]
 #![warn(rust_2018_idioms)]
 
-use assert_cmd::prelude::*;
+use assert_cmd::Command;
 use mkcert::AppError;
 use predicates::prelude::*;
-use std::process::Command;
 
 #[test]
 fn help() -> Result<(), AppError> {
@@ -37,6 +36,34 @@ fn version() -> Result<(), AppError> {
         .stdout(predicate::str::is_match(r"^mkcert \d+\.\d+\.\d+\n$")?);
     Ok(())
 }
+
+#[test]
+fn invalid_yaml_file() -> Result<(), AppError> {
+    let mut cmd = Command::cargo_bin("mkcert")?;
+    cmd.arg("--file").arg("README.adoc");
+    cmd.assert()
+        .failure()
+        .code(1)
+        .stderr(predicate::str::contains("Invalid YAML file"))
+        .stdout(predicate::str::is_empty());
+    Ok(())
+}
+
+// TODO: rpassword uses tty on Linux
+// #[test]
+// fn new_self_signed_cert() -> Result<(), AppError> {
+//     let mut cmd = Command::cargo_bin("mkcert")?;
+//     cmd.arg("--file")
+//         .arg("examples/self_signed.yaml")
+//         .write_stdin("pw123")
+//         .write_stdin("pw123");
+//     cmd.assert()
+//         .success()
+//         .code(0)
+//         .stderr(predicate::str::is_empty())
+//         .stdout(predicate::str::contains("test"));
+//     Ok(())
+// }
 
 //#[test]
 //fn new_cert_syntax() -> Result<(), AppError> {
